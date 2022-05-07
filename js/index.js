9cb3670f430117d482afe8d121ea206c095dede1
PrabhmeharSingh <99373449+PrabhmeharSingh@users.noreply.github.com>
2022-05-07 15:10:48 -0400

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+let device_width=0;
 let mod=false;
 function showlinks(ID)
 {
@@ -13,4 +14,20 @@ e.style.display="none";
 function mouseoverdropdown(x)
 {
 mod=x;
+}
+function setdevicewidth()
+{
+device_width=document.body.clientWidth;
+}
+function load()
+{
+setdevicewidth();
+let p=document.getElementById('imagehere');
+p.style.height=(device_width*840/1920)+"px";
+}
+function resize()
+{
+setdevicewidth();
+let p=document.getElementById('imagehere');
+p.style.height=(device_width*840/1920)+"px";
 }
